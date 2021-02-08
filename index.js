02024afdd1d0ff21165cee4a2a4cba3b8350bb81
esmasher <esmasherr@gmail.com>
2021-02-07 20:09:58 -0600

    commit tree 941c9fcd1f3dd663e2a3d143f3eb18a2ee91bf78

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+console.log(multiply)
+
+const random = Math.round(Math.random())+1
+console.log(random)
+
+const num3 = 16
+const num4 = 6
+const mod = (num3 % num4)
+console.log(mod)
+
+const max = Math.max(1,2,20)
+console.log(max)
